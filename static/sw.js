e57dc28417b36d7896cd0ce654da245a8265d7e8
RobinFrugte97 <robin.frugte@hotmail.com>
2020-03-16 15:25:47 +0100

    added service worker

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,48 @@
-self.addEventListener('install', event => {
-
-})
-
-self.addEventListener('active', event => {
-
-})
+self.addEventListener('install', event => event.waitUntil(
+    caches.open('msh-cache-v1')
+        .then(cache => cache.addAll([
+            '/home',
+            'style.css',
+            'dist/index.js',
+            'images/Marvel-Logo.png',
+            "offline/"
+        ]))
+        .then(self.skipWaiting())
+));
 
 self.addEventListener('fetch', event => {
-
+    const request = event.request
+    if (request.mode === 'navigate') {
+        event.respondWith(
+            fetch(request)
+                .then(response => cachePage(request, response))
+                .catch(err => getCachedPage(request))
+                .catch(err => fetchCoreFile('offline/'))
+        )
+    } else {
+        event.respondWith(
+            fetch(request)
+                .catch(err => fetchCoreFile(request.url))
+                .catch(err => fetchCoreFile('offline/'))
+        )
+    }
 })
+
+function fetchCoreFile(url) {
+    return caches.open('msh-cache-v1')
+        .then(cache => cache.match(url))
+        .then(response => response ? response : Promise.reject());
+}
+
+function getCachedPage(request) {
+    return caches.open('msh-cache-v1')
+        .then(cache => cache.match(request))
+        .then(response => response ? response : Promise.reject());
+}
+
+function cachePage(request, response) {
+    const clonedResponse = response.clone();
+    caches.open('msh-cache-v1')
+        .then(cache => cache.put(request, clonedResponse));
+    return response
+}
