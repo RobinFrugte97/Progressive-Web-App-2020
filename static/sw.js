0440b39bc79c990f2444d55f366d23dfadda6c89
RobinFrugte97 <robin.frugte@hotmail.com>
2020-03-16 18:28:52 +0100

    dummy sw to test

--- static/sw.js
+++ static/sw.js
@@ -1,51 +1,87 @@
-self.addEventListener('install', event => event.waitUntil(
-    caches.open('msh-cache-v1')
-        .then(cache => cache.addAll([
-            '/home',
-            'style.css',
-            'dist/index.js',
-            'images/Marvel-Logo.png',
-            "offline/"
-        ]))
-))
+const cacheName = 'cache-v1';
+const precacheResources = [
+        '/home',
+        'style.css',
+        'dist/index.js',
+        'images/Marvel-Logo.png',
+        "offline/"
+];
+
+self.addEventListener('install', event => {
+    console.log('Service worker install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(precacheResources);
+            })
+    );
+});
 
 self.addEventListener('activate', event => {
     console.log('Service worker activate event!');
-})
+});
 
 self.addEventListener('fetch', event => {
-    const request = event.request
-    if (request.mode === 'navigate') {
-        event.respondWith(
-            fetch(request)
-                .then(response => cachePage(request, response))
-                .catch(err => getCachedPage(request))
-                .catch(err => fetchCoreFile('offline/'))
-        )
-    } else {
-        event.respondWith(
-            fetch(request)
-                .catch(err => fetchCoreFile(request.url))
-                .catch(err => fetchCoreFile('offline/'))
-        )
-    }
-})
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
 
-function fetchCoreFile(url) {
-    return caches.open('msh-cache-v1')
-        .then(cache => cache.match(url))
-        .then(response => response ? response : Promise.reject());
-}
+// self.addEventListener('install', event => event.waitUntil(
+//     caches.open('msh-cache-v1')
+//     .then(cache => cache.addAll([
+//         '/home',
+//         'style.css',
+//         'dist/index.js',
+//         'images/Marvel-Logo.png',
+//         "offline/"
+//     ]))
+//     .then(self.skipWaiting())
+// ))
 
-function getCachedPage(request) {
-    return caches.open('msh-cache-v1')
-        .then(cache => cache.match(request))
-        .then(response => response ? response : Promise.reject());
-}
+// self.addEventListener('activate', event => {
+//     console.log('Service worker activate event!');
+// })
 
-function cachePage(request, response) {
-    const clonedResponse = response.clone();
-    caches.open('msh-cache-v1')
-        .then(cache => cache.put(request, clonedResponse));
-    return response
-}
+// self.addEventListener('fetch', event => {
+//     const request = event.request
+//     if (request.mode === 'navigate') {
+//         event.respondWith(
+//             fetch(request)
+//                 .then(response => cachePage(request, response))
+//                 .catch(err => getCachedPage(request))
+//                 .catch(err => fetchCoreFile('offline/'))
+//         )
+//     } else {
+//         event.respondWith(
+//             fetch(request)
+//                 .catch(err => fetchCoreFile(request.url))
+//                 .catch(err => fetchCoreFile('offline/'))
+//         )
+//     }
+// })
+
+// function fetchCoreFile(url) {
+//     return caches.open('msh-cache-v1')
+//         .then(cache => cache.match(url))
+//         .then(response => response ? response : Promise.reject());
+// }
+
+// function getCachedPage(request) {
+//     return caches.open('msh-cache-v1')
+//         .then(cache => cache.match(request))
+//         .then(response => response ? response : Promise.reject());
+// }
+
+// function cachePage(request, response) {
+//     const clonedResponse = response.clone();
+//     caches.open('msh-cache-v1')
+//         .then(cache => cache.put(request, clonedResponse));
+//     return response
+// }
