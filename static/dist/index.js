db5caabaf2a10bb1f343ed3896f91bc7a482a993
RobinFrugte97 <robin.frugte@hotmail.com>
2020-03-17 14:38:08 +0100

    dist fix

--- static/dist/index.js
+++ static/dist/index.js
@@ -1,4 +1,16 @@
-
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js')
+            .then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope)
+                return registration.update()
+            }, function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err)
+            })
+    })
+}
 
 // let filterInput = document.getElementById("filterInput");
 // let filteredCharacters = []
